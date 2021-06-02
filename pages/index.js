8bdb29e4cf8af673890edb84797638fd5d41bdd2
lewkhoonpeng <lewkhoonpeng@yahoo.com>
2021-06-02 15:17:47 +0100

    deleted home css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -14,14 +14,14 @@ export default function Home() {
       </Head>
 
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1 className={"title"}>Homepage</h1>
+        <p className={"text"}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iure,
           facere vitae beatae repellat sint, minus a corporis vel odit earum
           placeat recusandae quae quisquam culpa ratione unde reiciendis ex iste
           cum nam tenetur sed illo? Odio itaque beatae officiis?
         </p>
-        <p className={styles.text}>
+        <p className={"text"}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam fuga
           enim exercitationem reiciendis praesentium accusamus dignissimos vel
           mollitia accusantium cum harum, in repudiandae molestias officia
@@ -29,8 +29,29 @@ export default function Home() {
           Deserunt illum praesentium voluptates commodi odit.
         </p>
         <Link href="/webcreater">
-          <a className={styles.btn}>See WebCreater Listing</a>
+          <a className={"btn"}>See WebCreater Listing</a>
         </Link>
+
+        <style jsx>{`
+          .title {
+            color: #333;
+            padding-bottom: 20px;
+            text-align: center;
+          }
+          .text {
+            color: #777;
+          }
+          .btn {
+            display: block;
+            width: 150px;
+            padding: 8px 0;
+            margin: 20px auto;
+            background: #4979ff;
+            border-radius: 4px;
+            color: white;
+            text-align: center;
+          }
+        `}</style>
       </div>
     </>
   );
