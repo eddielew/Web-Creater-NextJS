64e2dcdec74882e77b5f11bb9fcbc6ad422bf758
lewkhoonpeng <lewkhoonpeng@yahoo.com>
2021-06-02 15:35:57 +0100

    deleted import header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-// import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
