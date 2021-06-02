eeb5acfc96c5e4893dd824b5144d9c82d560631a
lewkhoonpeng <lewkhoonpeng@yahoo.com>
2021-06-02 15:53:35 +0100

    cleanup files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
