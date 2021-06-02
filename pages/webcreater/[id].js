9a59f61a2b09faa3b81aae56e05b30763e356281
lewkhoonpeng <lewkhoonpeng@yahoo.com>
2021-06-02 16:11:08 +0100

    cleanup

--- pages/webcreater/[id].js
+++ pages/webcreater/[id].js
@@ -1,5 +1,3 @@
-import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
-
 export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
