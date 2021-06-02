8f959b158f6fe9a261d7d3aec28f73f978c14712
lewkhoonpeng <lewkhoonpeng@yahoo.com>
2021-06-02 15:26:18 +0100

    deleted webcreater css and added styled-jsx css

--- pages/webcreater/index.js
+++ pages/webcreater/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import styles from "../../styles/Webcreater.module.css";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -22,12 +21,25 @@ const Webcreater = ({ webcreaters }) => {
         <h1>All Web Creaters!</h1>
         {webcreaters.map((webcreater) => (
           <Link href={`/webcreater/${webcreater.id}`} key={webcreater.id}>
-            <a className={styles.single}>
+            <a className={"single"}>
               <h3>{webcreater.name}</h3>
             </a>
           </Link>
         ))}
       </div>
+      <style jsx>{`
+        .single {
+          padding: 2px 16px;
+          background: #fff;
+          display: block;
+          margin: 20px 10px;
+          border-left: 8px solid #fff;
+        }
+
+        .single:hover {
+          border-left: 8px solid #4979ff;
+        }
+      `}</style>
     </>
   );
 };
